1cf6e9123e416c3d66e62f3fb04840ad61654887
tjdbwns19 <tjdbwns19@naver.com>
2023-02-18 17:03:57 +0900

    [mod] README.md

---
+++ BeExpert/BeExpert/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeExpert
+//
+//  Created by 서유준 on 2023/02/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # BeExpert 개발일지
-
+## 2023.02.18
+- BeExpert 리포지토리 생성     
+- UI 구상      
+<img src="./img/plan.png" width="900" height="1000">
